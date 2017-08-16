4eea23c22b93083efc3b89993305e8fca60480d6
ZiyueHuang <zyhuang94@gmail.com>
2017-08-17 07:22:41 +0800

    attention

--- seq2seq.py
+++ seq2seq.py
@@ -4,7 +4,6 @@ import mxnet as mx
 import numpy as np
 import logging
 
-
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -42,7 +41,7 @@ target_dim = 10
 
 num_layers = 3
 num_hidden = 100
-num_embed = 50
+num_embed = 40
 
 encoder = mx.rnn.SequentialRNNCell()
 decoder = mx.rnn.SequentialRNNCell()
@@ -51,24 +50,24 @@ for i in range(num_layers):
     encoder.add(mx.rnn.LSTMCell(num_hidden, prefix='encoder_%d_' % i))
     decoder.add(mx.rnn.LSTMCell(num_hidden, prefix='decoder_%d_' % i))
 
+src_data = mx.sym.var('src_data')
+target_data = mx.sym.var('target_data')
+label = mx.sym.var('softmax_label')
+attn_fc_weight = mx.sym.var('attn_fc_weight')
+attn_fc_bias = mx.sym.var('attn_fc_bias')
 
+src_embed = mx.sym.Embedding(data=src_data, input_dim=src_dim, 
+                             output_dim=num_embed, name='src_embed') 
+target_embed = mx.sym.Embedding(data=target_data, input_dim=target_dim, 
+                                output_dim=num_embed, name='target_embed')
 def sym_gen(seq_len):
-    src_data = mx.sym.var('src_data')
-    target_data = mx.sym.var('target_data')
-    label = mx.sym.var('softmax_label')
-
-    src_embed = mx.sym.Embedding(data=src_data, input_dim=src_dim, 
-                                 output_dim=num_embed, name='src_embed') 
-    target_embed = mx.sym.Embedding(data=target_data, input_dim=target_dim, 
-                                    output_dim=num_embed, name='target_embed')
-
     enc_len, dec_len = seq_len
 
     encoder.reset()
     for cell in encoder._cells:
     	cell.reset()
 
-    encoder_outputs, encoder_states = encoder.unroll(enc_len, inputs=src_embed)
+    encoder_outputs, encoder_state = encoder.unroll(enc_len, inputs=src_embed, merge_outputs=True)
 
     decoder.reset()
     for cell in decoder._cells:
@@ -76,20 +75,43 @@ def sym_gen(seq_len):
 
     inputs, _ = _normalize_sequence(dec_len, target_embed, 'NTC', False)
 
-    states = encoder_states
+    state = encoder_state
     outputs = []
-    # TODO: attention
+
+    last_context = mx.sym.zeros((batch_size, num_hidden), name='before_first_context')
+    # TODO: refactor
     for i in range(dec_len):
-        decoder_output, states = decoder(inputs[i], states)
-        outputs.append(decoder_output)
+        dec_input = mx.sym.concat(inputs[i], last_context, dim=1)
+        # (b, n)
+        decoder_output, state = decoder(dec_input, state)
+        
+        fc_decoder_output = mx.sym.FullyConnected(data=decoder_output, weight=attn_fc_weight, bias=attn_fc_bias,
+                                                  num_hidden=num_hidden, name='fc_decoder_output_%d' % i)
+        # (b, 1, n)  n is num_hidden
+        fc_decoder_output = mx.sym.expand_dims(data=fc_decoder_output, axis=1,
+                                               name='expand_fc_decoder_output_%d' % i)
+        # (b, 1, n) (b, seq, n) => (b, 1, seq)
+        attn_weight = mx.sym.batch_dot(fc_decoder_output, encoder_outputs, 
+                                       transpose_b=True, name='attn_weight_%d' % i)
+        attn_weight = mx.sym.softmax(data=attn_weight, name='normalized_attn_weight_%d' % i)
+        # (b, 1, seq) (b, seq, n) => (b, 1, n)
+        last_context = mx.sym.batch_dot(attn_weight, encoder_outputs, 
+                                        name='context_%d' % i)
+        # (b, n)
+        last_context = mx.sym.reshape(data=last_context, shape=(0, -3), name='squeeze_context_%d' % i)
+
+        output = mx.sym.concat(decoder_output, last_context, dim=1, name='output_%d' % i)
+
+        outputs.append(output)
+
 
     outputs, _ = _normalize_sequence(dec_len, outputs, 'NTC', True)
 
     label_reshape = mx.sym.reshape(data=label, shape=(-1,), name='label_reshape')
 
-    outputs_reshape = mx.sym.reshape(outputs, shape=(-1, num_hidden), name='outputs_reshape')
-    pred = mx.sym.FullyConnected(data=outputs_reshape, num_hidden=target_dim, name='fc')
-    pred = mx.sym.SoftmaxOutput(data=pred, label=label_reshape, name='softmax')
+    outputs = mx.sym.reshape(outputs, shape=(-3, -1), name='outputs_reshape')
+    fc = mx.sym.FullyConnected(data=outputs, num_hidden=target_dim, name='fc')
+    pred = mx.sym.SoftmaxOutput(data=fc, label=label_reshape, name='softmax')
     # reshape for acc metric
     pred_reshape = mx.sym.reshape(data=pred, shape=(-1, dec_len, target_dim), name='pred_reshape')
 
@@ -100,12 +122,12 @@ batch_size = 5
 buckets = [(5, 10), (10, 20), (20, 40)]
 
 train_iter = DummySeq2seqIter(batch_size, buckets, ['src_data', 'target_data'], ['softmax_label'],
-                                     src_dim, target_dim)
+                              src_dim, target_dim)
 
 seq2seq_mod = mx.mod.BucketingModule( 
-        sym_gen            = sym_gen,
+        sym_gen = sym_gen,
         default_bucket_key = train_iter.default_bucket_key,
-        context            = mx.cpu(0))
+        context = mx.cpu(0))
 
 seq2seq_mod.fit(train_iter,
         optimizer='sgd',
