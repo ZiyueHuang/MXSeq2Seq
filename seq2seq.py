aa7d027843aa31223861d9ac9d40ffbf2a6913e7
ZiyueHuang <zyhuang94@gmail.com>
2017-08-21 12:59:42 +0800

    more attention

--- seq2seq.py
+++ seq2seq.py
@@ -4,6 +4,7 @@ import mxnet as mx
 import numpy as np
 import logging
 
+
 logging.getLogger().setLevel(logging.INFO)
 
 
@@ -36,6 +37,8 @@ def _normalize_sequence(length, inputs, layout, merge, in_layout=None):
     return inputs, axis
 
 
+batch_size = 5
+
 src_dim = 5
 target_dim = 10
 
@@ -55,11 +58,45 @@ target_data = mx.sym.var('target_data')
 label = mx.sym.var('softmax_label')
 attn_fc_weight = mx.sym.var('attn_fc_weight')
 attn_fc_bias = mx.sym.var('attn_fc_bias')
+attn_other = mx.sym.var('attn_other', shape=(1, num_hidden))
 
 src_embed = mx.sym.Embedding(data=src_data, input_dim=src_dim, 
                              output_dim=num_embed, name='src_embed') 
 target_embed = mx.sym.Embedding(data=target_data, input_dim=target_dim, 
                                 output_dim=num_embed, name='target_embed')
+
+def attention(method, enc_len, num_hidden, encoder_outputs, decoder_output):
+    if method == 'general':
+        left = decoder_output
+        out = mx.sym.reshape(data=encoder_outputs, shape=(-3, 0))
+        out = mx.sym.FullyConnected(data=out, weight=attn_fc_weight, 
+                                            bias=attn_fc_bias, num_hidden=num_hidden,
+                                            name='fc_decoder_output_%d' % i)
+        right = mx.sym.reshape(data=out, shape=(batch_size, -1, num_hidden))
+
+    elif method == 'dot':
+        left = decoder_output
+        right = encoder_outputs
+    elif method == 'concat':
+        out = mx.sym.expand_dims(data=decoder_output, axis=1)
+        out = mx.sym.tile(data=out, reps=(1, enc_len, 1))
+        out = mx.sym.concat(out, encoder_outputs, dim=2)
+        out = mx.sym.reshape(data=out, shape=(-1, 2*num_hidden))
+        out = mx.sym.FullyConnected(data=out, weight=attn_fc_weight, 
+                                            bias=attn_fc_bias, num_hidden=num_hidden)
+        right = mx.sym.reshape(data=out, shape=(batch_size, -1, num_hidden))
+        left = mx.sym.tile(data=attn_other, reps=(batch_size, 1))
+    else:
+        assert False
+
+    # (b, 1, n)  n is num_hidden
+    left = mx.sym.expand_dims(data=left, axis=1, name='expand_left_%d' % i)
+    # (b, 1, n) (b, seq, n) => (b, 1, seq)
+    attn_weight = mx.sym.batch_dot(left, right, transpose_b=True, name='attn_weight_%d' % i)
+    attn_weight = mx.sym.softmax(data=attn_weight, name='normalized_attn_weight_%d' % i)
+
+    return attn_weight
+
 def sym_gen(seq_len):
     enc_len, dec_len = seq_len
 
@@ -79,24 +116,16 @@ def sym_gen(seq_len):
     outputs = []
 
     last_context = mx.sym.zeros((batch_size, num_hidden), name='before_first_context')
-    # TODO: refactor
+    
     for i in range(dec_len):
         dec_input = mx.sym.concat(inputs[i], last_context, dim=1)
         # (b, n)
         decoder_output, state = decoder(dec_input, state)
-        
-        fc_decoder_output = mx.sym.FullyConnected(data=decoder_output, weight=attn_fc_weight, bias=attn_fc_bias,
-                                                  num_hidden=num_hidden, name='fc_decoder_output_%d' % i)
-        # (b, 1, n)  n is num_hidden
-        fc_decoder_output = mx.sym.expand_dims(data=fc_decoder_output, axis=1,
-                                               name='expand_fc_decoder_output_%d' % i)
-        # (b, 1, n) (b, seq, n) => (b, 1, seq)
-        attn_weight = mx.sym.batch_dot(fc_decoder_output, encoder_outputs, 
-                                       transpose_b=True, name='attn_weight_%d' % i)
-        attn_weight = mx.sym.softmax(data=attn_weight, name='normalized_attn_weight_%d' % i)
-        # (b, 1, seq) (b, seq, n) => (b, 1, n)
+
+        attn_weight = attention('general', enc_len, num_hidden, encoder_outputs, decoder_output)
+
         last_context = mx.sym.batch_dot(attn_weight, encoder_outputs, 
-                                        name='context_%d' % i)
+                                        name='context_%d' % i)       
         # (b, n)
         last_context = mx.sym.reshape(data=last_context, shape=(0, -3), name='squeeze_context_%d' % i)
 
@@ -118,7 +147,6 @@ def sym_gen(seq_len):
     return pred_reshape, ('src_data', 'target_data'), ('softmax_label',)
 
 
-batch_size = 5
 buckets = [(5, 10), (10, 20), (20, 40)]
 
 train_iter = DummySeq2seqIter(batch_size, buckets, ['src_data', 'target_data'], ['softmax_label'],
